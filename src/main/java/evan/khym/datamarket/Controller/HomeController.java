30dc56134057483decf7a9df028987e2e488c4bc
evkhym <ekhym24@groton.org>
2023-01-02 22:57:52 -0500

    Initial commit

--- src/main/java/evan/khym/datamarket/Controller/HomeController.java
+++ src/main/java/evan/khym/datamarket/Controller/HomeController.java
@@ -10,5 +10,30 @@ public class HomeController {
     public String home() {
         return "index";
     }
-
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+    @GetMapping("/board")
+    public String board() {
+        return "board";
+    }
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+    @GetMapping("/datasets")
+    public String datasets() {
+        return "datasets";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @GetMapping("/message")
+    public String message() {
+        return "message";
+    }
+    @GetMapping("/upload")
+    public String upload() { return "upload"; }
 }
